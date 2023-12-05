9153d096dfe9fb6ae208d25dd05fd1a77696852e
Archit Soni <archit2@ualberta.ca>
2023-12-05 14:40:43 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import Papa from 'papaparse';
 import ReactDOM from 'react-dom';
 import ReactHlsPlayer from 'react-hls-player';
 
-function callAPI(){
+function callAPI(duration_secs, title){
+  document.getElementById(title).classList.add('uk-card-secondary');
   let request = new XMLHttpRequest();
-    let url = "https://cj2j4ag3yf.execute-api.us-east-1.amazonaws.com/InsertAdMarker?duration=60"
+    let url = "https://cj2j4ag3yf.execute-api.us-east-1.amazonaws.com/InsertAdMarker?duration="
+    url = url + duration_secs
     request.open("POST", url);
     request.setRequestHeader("Content-Type", "application/json");
     request.send(JSON.stringify({}));
+  printTime(duration_secs);
 }
+
+function printTime(duration_secs) {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  let timeString = hours + ':' + minutes + ':' + seconds + " for " + duration_secs + " seconds";
+
+  document.getElementById('time').innerText = timeString;
+}
+
 function App() {
   const [csvData, setCsvData] = useState([]);
   const logs = []
@@ -32,15 +47,17 @@ function App() {
           console.log(results.data[i][0])
           var logposdur = []
           var str = ""
-          var log = results.data[i][0]
-          var pos = results.data[i][1]
-          var dur = results.data[i][8]
-          str = log+pos+dur
+          var log = results.data[i][1]
+          var pos = results.data[i][2]
+          var dur = results.data[i][9]
+          var dur_sec = results.data[i][10]
+          str = log+pos+dur+dur_sec
           if(!strings.includes(str)){
             strings.push(str)
             logposdur.push(log)
             logposdur.push(pos)
             logposdur.push(dur)
+            logposdur.push(dur_sec)
             console.log(logposdur)
             console.log(logs)
             logs.push(logposdur)
@@ -52,32 +69,57 @@ function App() {
   }
 
   return (
-    
-    <div className="videos" style={{marginTop:20}}>
+    <div>
+      <div uk-grid class="uk-background-primary uk-padding"  style={{backgroundColor:'black', borderBottom:'2px solid white'}} >
+        <div><img src="https://m.media-amazon.com/images/G/01/digital/video/web/Logo-min.png"></img>
+        <span style={{paddingLeft:'60%', fontSize:46, color:'#eee'}}>Cloud PCR
+        </span>
+        </div>
+        
+    </div>
+    <div uk-grid>
+        <div class="uk-width-1-1" style={{paddingTop:'20px', paddingBottom:'1px', position:'relative', marginLeft:'42%', marginBottom:'5%'}}>
+        <h3 class="uk-text-center" style={{backgroundColor:'rgba(255, 255, 255, 0.9)', padding: '0.5em', position:'absolute'}}>Red Sox @ Yankees</h3></div>
+    </div>
+    <div className="videos" style={{marginTop:'3%', position:'relative', left:'8%'}}>
       <ReactHlsPlayer
     src="https://d323vnyfir0xwq.cloudfront.net/out/v1/685efb6747b2444fb3078345ab88e502/index.m3u8"
     autoPlay={false}
     controls={true}
-    width="30%"
+    width="40%"
     height="auto"
-    style={{marginRight:20}}
+    style={{marginRight:20, border:'2px solid green'}}
   />
   <ReactHlsPlayer
-src="https://611681dbd22d40219f207d3f138674f8.mediatailor.us-east-1.amazonaws.com/v1/master/a6a26923d6c11d31f0be7e64a4cd86647c5565d8/medialiveoutput/index.m3u8"
+src="https://611681dbd22d40219f207d3f138674f8.mediatailor.us-east-1.amazonaws.com/v1/master/a6a26923d6c11d31f0be7e64a4cd86647c5565d8/medialiveoutput/index.m3u8?ads.param1=male&ads.param2=Sports,Nature,Food,Cars,"
 autoPlay={false}
 controls={true}
-width="30%"
+width="40%"
 height="auto"
+style={{border:'2px solid white'}}
 />
 <p></p>
-<h3 style={{display:'inline',
-marginRight:500}}>MediaLive Output</h3><h3 style={{display:'inline'}}>MediaTailor Output</h3>
+<h3 style={{display:'inline',color:'white',
+marginRight:500}}>MediaLive Output</h3><h3 style={{display:'inline',color:'white'}}>MediaTailor Output</h3>
 <p style={{display:'block'}}></p>
 <div className="file">
           <input type="file" onChange={handleFileUpload}/>
+          <div style={{border: "3px solid red", width:'fit-content', background:'black', color:'white', fontSize:24, padding:'0.5em', marginTop:'1%'}}>
+          <div>Ads Fired at:</div><span id="time"></span></div>
+        <div class="uk-accordion-content uk-child-width-1-4@m uk-grid-small uk-grid-match" uk-grid style={{display:'flex', flexWrap:'wrap'}} >
           {finalLogs.map(item => (
-        <div style={{margin:20, marginRight:800, paddingLeft:50, paddingBottom:30, borderStyle:'solid'}}key={item}><h4 onClick={callAPI}>{item[0]}{'\t'}{item[1]}{'\t'}</h4>{item[2]}</div>
-      ))}
+
+<div onClick={() => callAPI(item[3], item[0]+item[1]+item[3])}>
+          <div id={item[0]+item[1]+item[3]} class="uk-card uk-card-default uk-card-body" style={{marginBottom:"5%"}}>
+              <h3 class="uk-card-title"> {item[0]}{'\t'}{item[1]}{'\t'}({item[2]})</h3>
+          </div>
+          </div>
+
+
+            
+       
+      ))}</div>
+    </div>
     </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.23/dist/css/uikit.min.css" />
     <App />
   </React.StrictMode>
 );
